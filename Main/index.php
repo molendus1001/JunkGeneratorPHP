5f50de11171d6810c29dc5d1edfd210c68fb9163
Matthew <molendus1001@users.noreply.github.com>
2019-09-29 22:20:50 +0200

    Big update: Mini Gui, Switchable styles
    1. Buttons for simpler ussage
    2. Button text is unselectable
    3. Added case-junk

--- Main/index.php
+++ Main/index.php
@@ -1,6 +1,23 @@
+<style>
+/* https://stackoverflow.com/questions/2310734/how-to-make-html-text-unselectable */
+.unselectable {
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+</style>
+<div class="unselectable">
+<a href=index.php?m=0><button type="button">Generate Switch/Case[int] Junk</button></a>
+<a href=index.php?m=1><button type="button">Generate Passive if statement Junk</button></a>
+<br><br>
+</div>
+
 <?php
-$i = 0;                                     //Line counter to begin with
-function generateRandomString($length = 10) { //source: https://stackoverflow.com/questions/4356289/php-random-string-generator
+$i = 0;											//Line counter to begin with
+function generateRandomString($length = 99) {	//source: https://stackoverflow.com/questions/4356289/php-random-string-generator
     $characters = '._-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
     $randomString = '';
@@ -9,13 +26,25 @@ function generateRandomString($length = 10) { //source: https://stackoverflow.co
     }
     return $randomString;
 }
-echo "if (true == false) { ";               //This Code will never execute, and it will not use cpu.
-while( $i < 999) {                          //number of loops
-echo "char Junk";                           //name of char, we just keep them simillar, becouse compiler kicks exact variable names anyway
-echo $i;                                    //echo number of line - so that we don't have two identical variables
-echo "[1024] = \"";                         //maximum memsize of char
-echo generateRandomString(rand(900, 1020)); //length of random string, edit rand to pick minimum and maximum random length, going above 1024 will result in compilator errors unless you change maximum value in line above
-echo "\";<br>";                             //visual studio don't like when you use one very long line, it woud be likely to freeze
-$i++;}
-echo "}";
+if ($_GET["m"] == 1) {
+	echo "case 2147483646:<br>";			//Code after this statement will never execute, and it will not use cpu.
+}
+else {
+	echo "if (true == false) { <br>";		//This case uses very big number that is unlikely to be used in normal cases
+}
+while( $i < 99) {								//number of loops
+	echo "char Junk";							//name of char, we just keep them simillar, becouse compiler kicks exact variable names anyway
+	echo $i;									//echo number of line - so that we don't have two identical variables
+	echo "[1024] = \"";							//maximum memsize of char
+	echo generateRandomString(rand(900, 1020));	//length of random string, edit rand to pick minimum and maximum random length, going above 1024 will result in compilator errors unless you change maximum value in line above
+	echo "\";<br>";								//visual studio don't like when you use one very long line, it woud be likely to freeze
+	$i++;
+}
+if ($_GET["m"] == 1) {
+	echo "break;";
+}
+else {
+	echo "}";
+}
+
 ?>
